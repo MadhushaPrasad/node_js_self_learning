45ccae82668103f24477a20a082cf44513a16505
MadhushaPrasad <madushaprasad21@gmail.com>
2021-06-20 18:38:06 +0530

    test os module methods

--- test/core_test.js
+++ test/core_test.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+// const fs = require('fs');
 
 // fs.writeFileSync('data.txt', 'Hello JavaScript'); //file create and write
 // fs.appendFileSync('data.txt', ' Node js');//file create and append data to created file
@@ -6,3 +6,8 @@ const fs = require('fs');
 // const data = fs.readFileSync('data.txt'); //data read from created file
 // console.log(data); //data read as buffer
 // console.log(data.toString()); //data read as string
+
+const os = require('os');
+
+console.log(os.freemem()); //check free memory
+console.log(os.totalmem()); //check total memory
