ecfa8190551d0bd2d2edbe574b04b6511c86392d
MadhushaPrasad <madushaprasad21@gmail.com>
2021-06-20 18:27:42 +0530

    node js file writing and reading

---
+++ test/core_test.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+// fs.writeFileSync('data.txt', 'Hello JavaScript'); //file create and write
+// fs.appendFileSync('data.txt', ' Node js');//file create and append data to created file
+
+// const data = fs.readFileSync('data.txt'); //data read from created file
+// console.log(data); //data read as buffer
+// console.log(data.toString()); //data read as string
