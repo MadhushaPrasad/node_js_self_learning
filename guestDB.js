a1a88dfedb5881f0a5ef6ee536ca674513652b29
MadhushaPrasad <madushaprasad21@gmail.com>
2021-06-20 19:54:53 +0530

    change script of package.json file;

--- guestDB.js
+++ guestDB.js
@@ -1,11 +1,11 @@
 const chalk = require('chalk');
 
 const add = () => {
-  console.log(chalk.green('Adding'));
+  console.log(chalk.red('Adding'));
 };
 
 //View  ------ arrow function as  short
-const view = () => console.log(chalk.green('View'));
+const view = () => console.log(chalk.blue('View'));
 
 module.exports = {
   add,
