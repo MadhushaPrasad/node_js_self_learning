370ea578ec96b0940892385985788fab2b25dce2
MadhushaPrasad <madushaprasad21@gmail.com>
2021-06-21 12:47:15 +0530

    change gitignore

--- guestDB.js
+++ guestDB.js
@@ -1,13 +1,1 @@
 const chalk = require('chalk');
-
-const add = () => {
-  console.log(chalk.red('Adding'));
-};
-
-//View  ------ arrow function as  short
-const view = () => console.log(chalk.blue('View'));
-
-module.exports = {
-  add,
-  view,
-};
