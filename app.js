a3d4b351e9bbe672f3f0b0b1e112c777e531fd18
MadhushaPrasad <madushaprasad21@gmail.com>
2021-06-21 13:52:47 +0530

    setup yargs commands

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const yargs = require("yargs");
 const db = require('./guestDB');
 
-console.log(process.argv);
+yargs.version("1.2.0");
+
+yargs.command({
+    command: 'add',
+    describe: "Add a guest",
+    handle: function () {
+        db.addGuest();
+    }
+});
+
+yargs.command({
+    command: 'update',
+    describe: "update a guest",
+    handle: function () {
+        db.updateGuest();
+    }
+});
+
 console.log(yargs.argv);
 
