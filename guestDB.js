a6f4ea59d5228db37023de4b89317348883f547f
MadhushaPrasad <madushaprasad21@gmail.com>
2021-06-21 15:24:50 +0530

    implement saveGuest function iin guestDB.js

--- guestDB.js
+++ guestDB.js
@@ -1,4 +1,7 @@
 const chalk = require('chalk');
+const fs = require('fs');
+
+const data_file = "data.json";//global variable
 
 const addGuest = (dataObject) => {
     console.log(chalk.green("Add Guest ", dataObject.name, dataObject.address));
@@ -21,6 +24,11 @@ const getAllGest = () => {
     console.log(chalk.magenta("Get All Guest"));
 };
 
+//save data to json file
+const saveGuest = (guest) => {
+    const dataJson = JSON.stringify(guest);//stringify object to string
+    fs.writeFileSync(dataJson, dataJson);//write data to dataJson file
+};
 
 module.exports = {
     addGuest, updateGuest, deleteGuest, searchGest, getAllGest
